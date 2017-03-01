5140bb01b74b178796a03cbf291a329d28792f71
qqkillqq <qqkillqq@163.com>
2017-03-01 15:51:53 +0800

    更新formJS文件

--- js/forms.js
+++ js/forms.js
@@ -4,96 +4,119 @@
 $('document').ready(function(){
     var FILE_NAME_SUGGEST = "例：listing";
     //请求后台接口
+    var model = new Vue({
+        el: 'body',
+        data: {
+            showModelConfig:false,
+            phoneModelConfig:{
+                show:false,
+                item:null
+            },
+            customModels:[],
+            title:'',
+            fileList: {},
+            componentList: {},
+            fileName: '',
+            fileNameSuggest: FILE_NAME_SUGGEST,
+            filePath: '',
+            midway: '',
+            comp :'',
+            isWcShare: false,
+            isAppShare: false,
+            itemList:[],
+            downloadLink:'',
+            listCom:[]
+        },
+        watch: {
+            itemList: function(e){
+
+
+            },
+            title: function(val){
+                var that = this;
+                translateWord(val, function(d){
+                    var result = d && d.trans_result && d.trans_result.map(function(i){return i.dst}).join(','),
+                        enFileNameList = result.split(' ');
+                    that.$data.fileNameSuggest =  '翻译建议：'+result;
+
+                    for(var i=0;i<enFileNameList.length;i++){
+                        if(i === 0 ){
+                            enFileNameList[i]=enFileNameList[i].charAt(0).toLowerCase()+enFileNameList[i].substring(1)
+                        }else{
+                            enFileNameList[i]=enFileNameList[i].charAt(0).toUpperCase()+enFileNameList[i].substring(1);
+                        }
+                    }
+                    that.$data.fileName = enFileNameList.join('');
+                })
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+            ready: function (e) {
+                console.log(e)
+            },
+            submit: function(e){
+                var that = this;
+                //阻止默认事件
+                e.preventDefault();
+                var o = {
+                    title: that.$data.title,
+                    fileName: that.$data.fileName,
+                    filePath: that.$data.filePath,
+                    midway: that.$data.midway,
+                    isWcShare: that.$data.isWcShare,
+                    isAppShare: that.$data.isAppShare,
+                    comp: that.$data.comp,
+                    listCom: that.$data.listCom,
+                    itemList: that.$data.itemList
+                };
+                console.log('--------submit---------');
+                console.log(o);
+                console.log('--------submit---------');
+                $.ajax({
+                    url: 'http://10.13.1.49:8082/make',
+                    method: 'GET',
+                    data: {options:o},
+                    success: function(data){
+                        console.log('http://10.13.1.49:8082/download?fileName='+data.fileName);
+                        that.$data.downloadLink = 'http://10.13.1.49:8082/download?fileName='+data.fileName;
+                        setTimeout(function(){
+                            window.open('http://10.13.1.49:8082/download?fileName='+data.fileName)
+
+                        },500)
+                        //history.replaceState(false, document.title,'http://10.13.1.49:8082/download?fileName='+data.fileName)
+                    }
+                });
+                //$.get('http://10.13.1.49:8082/make', {options: o}, function(fileName){
+                //    debugger
+                //    if(fileName){
+                //        history.replaceState('http://10.13.1.49:8082/download?fileName='+fileName)
+                //    }
+                //})
+            },
+            addPhoneModel:function(e){
+                var len=this.customModels.length;
+                this.customModels.push({title:Math.random().toString(36).substr(2,5)})
+            },
+            configModel:function(item){
+                this.phoneModelConfig.show=true;
+                this.phoneModelConfig.item=item
+            },
+            configModelOK:function(){
+                this.phoneModelConfig.show=false;
+                
+            },
+            configModelCancel:function(){
+                this.phoneModelConfig.show=false;
+            }
+        }
+    })
     $.get('http://10.13.1.49:8082/start', function (httpData) {
         var data = httpData.data;
-        var model = new Vue({
-            el: 'body',
-            data: {
-                title:'',
-                fileList: data.jsList,
-                componentList: data.componentList,
-                fileName: '',
-                fileNameSuggest: FILE_NAME_SUGGEST,
-                filePath: '',
-                midway: '',
-                comp :'',
-                isWcShare: false,
-                isAppShare: false,
-                itemList:[],
-                downloadLink:'',
-                topRightBtn:['shoppingCart','menuControl'],     //右上角外露按钮列表
-                listCom:[]
-            },
-            watch: {
-                itemList: function(e){
-
-
-                },
-                filePath: function(val){
-                    if(!this.$data.midway){
-                        this.$data.midway = '/' + val + '/' + this.$data.fileName + '/init.node';
-                    }
-                },
-                title: function(val){
-                    var that = this;
-                    translateWord(val, function(d){
-                        var result = d && d.trans_result && d.trans_result.map(function(i){return i.dst}).join(','),
-                            enFileNameList = result.split(' ');
-                        that.$data.fileNameSuggest =  '翻译建议：'+result;
-
-                        for(var i=0;i<enFileNameList.length;i++){
-                            if(i === 0 ){
-                                enFileNameList[i]=enFileNameList[i].charAt(0).toLowerCase()+enFileNameList[i].substring(1)
-                            }else{
-                                enFileNameList[i]=enFileNameList[i].charAt(0).toUpperCase()+enFileNameList[i].substring(1);
-                            }
-                        }
-                        that.$data.fileName = enFileNameList.join('');
-                    })
-                }
-            },
-            computed: {
-
-            },
-            methods: {
-                ready: function (e) {
-                    console.log(e)
-                },
-                submit: function(e){
-                    var that = this;
-                    //阻止默认事件
-                    e.preventDefault();
-                    var o = {
-                        title: that.$data.title,
-                        fileName: that.$data.fileName,
-                        filePath: that.$data.filePath,
-                        midway: that.$data.midway,
-                        isWcShare: that.$data.isWcShare,
-                        isAppShare: that.$data.isAppShare,
-                        comp: that.$data.comp,
-                        listCom: that.$data.listCom,
-                        topRightBtn: that.$data.topRightBtn,
-                        itemList: that.$data.itemList
-                    };
-                    console.log('--------submit---------');
-                    console.log(o);
-                    console.log('--------submit---------');
-                    $.ajax({
-                        url: 'http://10.13.1.49:8082/make',
-                        method: 'GET',
-                        data: {options:o},
-                        success: function(data){
-                            console.log('http://10.13.1.49:8082/download?fileName='+data.fileName);
-                            that.$data.downloadLink = 'http://10.13.1.49:8082/download?fileName='+data.fileName;
-                            setTimeout(function(){
-                                window.open('http://10.13.1.49:8082/download?fileName='+data.fileName)
-
-                            },500)
-                        }
-                    });
-                }
-            }
-        })
+        model.componentList=data.componentList;
+        model.fileList=data.jsList
     });
 
     function translateWord(word, callback){
